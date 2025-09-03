2383b3225cafb3dda3885d7cac70d1adc7718cbd
phayungsak.p.oga <phayungsak.p@oga.co.th>
2025-09-03 09:10:24 +0700

    refactor: Clean up SecureStorage.js by removing commented-out code

--- BS-Web/Frontend-Core/src/utils/SecureStorage.js
+++ BS-Web/Frontend-Core/src/utils/SecureStorage.js
@@ -1,32 +1,3 @@
-// import SecureLS from 'secure-ls';
-
-// const ls = new SecureLS({ encodingType: 'aes' });
-
-// const SecureStorage = {
-//   set: (key, value) => {
-//     ls.set(key, value);
-//   },
-
-//   get: (key) => {
-//     try {
-//       return ls.get(key);
-//     } catch (e) {
-//       console.warn(`Error getting key ${key} from SecureLS`, e);
-//       return null;
-//     }
-//   },
-
-//   remove: (key) => {
-//     ls.remove(key);
-//   },
-
-//   clear: () => {
-//     ls.clear();
-//   }
-// };
-
-// export default SecureStorage;
-
 import SecureLS from "secure-ls";
 import Logger from "./logger";
 
