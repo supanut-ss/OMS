22e7b0f2e018e35c7dae03fe326a08848f3899df
Thawatchai Prachumphan <thawatchai_p@oga.co.th>
2025-09-03 11:16:49 +0700

    add resouce api

---
+++ BS-API-Secure/Authentication/Models/Requests/ResourceRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Models.Requests
+{
+    public class ResourceRequest
+    {
+        [Required]
+        public string appliceation_license { get; set; } = string.Empty;
+        [Required]
+        public string platform { get; set; } = string.Empty;
+    }
+}
