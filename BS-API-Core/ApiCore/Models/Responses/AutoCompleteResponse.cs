7915717f13549af92ed539c7483d19866001d778
Thawatchai Prachumphan <thawatchai_p@oga.co.th>
2025-09-16 17:10:03 +0700

    update bs autocomplete

--- BS-API-Core/ApiCore/Models/Responses/AutoCompleteResponse.cs
+++ BS-API-Core/ApiCore/Models/Responses/AutoCompleteResponse.cs
@@ -4,11 +4,6 @@
     {
         public string message_code { get; set; } = "0";
         public string message_text { get; set; } = "success";
-        public List<AutoCompleteItem>? data { get; set; } = new List<AutoCompleteItem>();
-    }
-    public class AutoCompleteItem
-    {
-        public string code { get; set; } = string.Empty;
-        public string value { get; set; } = string.Empty;
+        public List<Dictionary<string, object>>? data { get; set; } = new List<Dictionary<string, object>>();
     }
 }
