2d26add8ff8494010eb989b2585b4c211c1f4640
phornphom <phornphom_ae@hotmail.com>
2025-10-14 10:28:12 +0700

    add docker

--- BS-Import-Export-Manager/Import-Export-Manager/Controllers/ImportMasterController.cs
+++ BS-Import-Export-Manager/Import-Export-Manager/Controllers/ImportMasterController.cs
@@ -1,5 +1,7 @@
 ﻿using Import_Export_Manager.Interfaces;
 using Import_Export_Manager.Models.Requests;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,6 +9,7 @@ namespace Import_Export_Manager.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ImportMasterController : ControllerBase
     {
         private readonly IImportMaster _importMasterService;
