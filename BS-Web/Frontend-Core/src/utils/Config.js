1c0d62354af290453917c1e32a71ff86a523ada8
Thawatchai Prachumphan <thawatchai_p@oga.co.th>
2025-10-21 09:13:15 +0700

    แก้ไข บัคเวลาเด้งไปหน้า login

--- BS-Web/Frontend-Core/src/utils/Config.js
+++ BS-Web/Frontend-Core/src/utils/Config.js
@@ -2,7 +2,9 @@ const Config = {
   API_URL: process.env.REACT_APP_API_URL,
   LICENSE_KEY: process.env.REACT_APP_API_LICENSE_KEY,
   ENCRYPYION: process.env.REACT_APP_ENCRYPYION,
-  APP_NAME: process.env.REACT_APP_NAME
+  APP_NAME: process.env.REACT_APP_NAME,
+  BASE_URL: process.env.REACT_APP_BASE_URL,
+  
 };
 
 export default Config;
