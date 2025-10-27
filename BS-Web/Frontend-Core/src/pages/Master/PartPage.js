8e11b6d65a2ca6dc0dfbc795fd12bb26587cfe1e
Rattakit <55879598+Rattakit@users.noreply.github.com>
2025-10-27 14:23:52 +0700

    master PartPage

--- BS-Web/Frontend-Core/src/pages/Master/PartPage.js
+++ BS-Web/Frontend-Core/src/pages/Master/PartPage.js
@@ -3,32 +3,33 @@ import { Typography, Paper } from "@mui/material";
 import BSDataGrid from "../../components/BSDataGrid";
 
 const PartPage = () => {
-  return (
-    <Paper sx={{ p: 3, mb: 3 }}>
-      <Typography variant="h6" gutterBottom>
-        Master Part
-      </Typography>
-      {/* ตารางข้อมูล */}
-      <BSDataGrid
-        // Enhanced Stored Procedure Configuration
-        bsStoredProcedure="usp_tbm_part"
-        bsStoredProcedureSchema="ams"
-        bsStoredProcedureParams={
-          {
-            // เพิ่มพารามิเตอร์เพิ่มเติมได้ตามต้องการ
-            // AreaCodeFilter: 'A01',
-            // SupplierFilter: 'Toyota',
-          }
-        }
-        // Standard Configuration
-        bsLocale="en"
-        bsAllowAdd={true}
-        bsAllowEdit={true}
-        bsAllowDelete={true}
-        bsPageSize={25}
-      />
-    </Paper>
-  );
+    return (
+        <Paper sx={{ p: 3, mb: 3 }}>
+            <Typography variant="h6" gutterBottom>
+                Master Part
+            </Typography>
+            {/* ตารางข้อมูล */}
+            <BSDataGrid
+                // Enhanced Stored Procedure Configuration
+                bsStoredProcedure="usp_tbm_part"
+                bsStoredProcedureSchema="ams"
+                bsCols="part_no,part_name,supplier_name,unit_price,snp,area_code,area_name,qty,create_by,create_date,update_by,update_date"
+                // bsStoredProcedureParams={
+                //     {
+                // เพิ่มพารามิเตอร์เพิ่มเติมได้ตามต้องการ
+                // AreaCodeFilter: 'A01',
+                // SupplierFilter: 'Toyota',
+                //     }
+                // }
+                // Standard Configuration
+                bsLocale="en"
+                // bsAllowAdd={true}
+                bsAllowEdit={true}
+                bsAllowDelete={true}
+                bsPageSize={25}
+            />
+        </Paper>
+    );
 };
 
 export default PartPage;
