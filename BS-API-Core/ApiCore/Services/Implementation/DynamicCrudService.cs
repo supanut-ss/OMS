c2414dd20b1322b4fa0a1e6134f65bb580266ede
phayungsakp <phayungsak.p@oga.co.th>
2025-10-27 15:41:09 +0700

    Update DynamicCrudService.cs

--- BS-API-Core/ApiCore/Services/Implementation/DynamicCrudService.cs
+++ BS-API-Core/ApiCore/Services/Implementation/DynamicCrudService.cs
@@ -840,7 +840,7 @@ namespace ApiCore.Services.Implementation
                     {
                         var fieldName = reader.GetName(i);
                         var value = reader.IsDBNull(i) ? null : reader.GetValue(i);
-                        data[fieldName] = value;
+                        data[fieldName] = value ?? DBNull.Value;
                     }
 
                     rows.Add(new DynamicResponse
@@ -1194,7 +1194,8 @@ namespace ApiCore.Services.Implementation
                 _logger.LogInformation("Executing Enhanced Stored Procedure: {ProcedureName}.{SchemaName} with operation: {Operation}",
                     request.ProcedureName, request.SchemaName, request.Operation);
 
-                using var connection = await _connectionFactory.CreateConnectionAsync();
+                using var connection = _connectionFactory.CreateConnection();
+                await connection.OpenAsync();
                 using var command = connection.CreateCommand();
 
                 // Build stored procedure call
@@ -1228,7 +1229,7 @@ namespace ApiCore.Services.Implementation
                 {
                     foreach (var param in request.Parameters)
                     {
-                        command.Parameters.Add(new SqlParameter($"@{param.Key}", ConvertParameterValue(param.Value)));
+                        command.Parameters.Add(new SqlParameter($"@{param.Key}", ConvertJsonElementValue(param.Value)));
                     }
                 }
 
