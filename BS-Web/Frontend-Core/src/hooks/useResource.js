ad417c2955af422e42120cd7891f19376669f0b2
Thawatchai Prachumphan <thawatchai_p@oga.co.th>
2025-11-11 12:50:56 +0700

    update code

---
+++ BS-Web/Frontend-Core/src/hooks/useResource.js
@@ -0,0 +1,29 @@
+import secureStorage from "../utils/SecureStorage";
+
+export const useResource = () => {
+
+    const getResource = (resourceData, resource_name) => {
+        let response = null;
+        if (resourceData) {
+            response = resourceData?.find(r => r.resource_name === resource_name)?.resource_value ?? resource_name;
+        }
+        return response;
+    }
+    const getResources = async (resource_group) => {
+        let response = [];
+        const resourceData = secureStorage.get("resource") || null;
+        const lang = secureStorage.get("lang");
+        if (resourceData) {
+            let data = [];
+            data = resourceData.filter(r => r.resource_group === resource_group);
+            data.map(item =>
+                response.push({
+                    resource_name: item.resource_name,
+                    resource_value: lang === "en" ? item.resource_en : lang === "th" ? item.resource_th : item.resource_other
+                })
+            );
+        }
+        return response;
+    }
+    return { getResource, getResources };
+}
