f6f9ae56ef6fd8b7863ab16737941baf21386a97
phornphom <phornphom_ae@hotmail.com>
2025-11-14 17:05:21 +0700

    ขยายขนาด col

--- BS-Web/Frontend-Core/src/pages/Import/ImportExcel.js
+++ BS-Web/Frontend-Core/src/pages/Import/ImportExcel.js
@@ -27,19 +27,19 @@ const ImportExcel = () => {
     {
       field: "code",
       headerName: "Code",
-      width: 150,
+      width: 120,
       type: "string",
     },
     {
       field: "message",
       headerName: "Message",
-      width: 600,
+      width: 700,
       type: "string",
     },
     {
       field: "records",
       headerName: "Records",
-      width: 90,
+      width: 350,
       type: "string",
     },
   ];
