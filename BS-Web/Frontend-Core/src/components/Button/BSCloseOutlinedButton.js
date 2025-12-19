19e3ecb065df6966c3d9ed2563066ca03b9f841e
phayungsakp <phayungsak.p@oga.co.th>
2025-12-19 10:29:09 +0700

    feat: Add new styled Save and Close outlined button components.

--- BS-Web/Frontend-Core/src/components/Button/BSCloseOutlinedButton.js
+++ BS-Web/Frontend-Core/src/components/Button/BSCloseOutlinedButton.js
@@ -3,22 +3,21 @@ import Button from '@mui/material/Button';
 
 const BSCloseOutlinedButton = styled(Button)({
   borderColor: '#F33838',
-    color: '#F33838',
-
-    '&:hover': {
-      borderColor: '#E00000',
-      color: '#E00000',
-      backgroundColor: 'transparent'
-    },
-
-    '&.Mui-focusVisible': {
-      borderColor: '#C80000',
-      color: '#C80000'
-    },
-
-    '&:active': {
-      borderColor: '#B00000',
-      color: '#B00000'
-    }
+  color: '#F33838',
+  '&:hover': {
+    borderColor: '#F33838',
+    backgroundColor: '#F33838',
+    color: '#FFFFFF',
+  },
+  '&.Mui-focusVisible': {
+    borderColor: '#C80000',
+    backgroundColor: '#C80000',
+    color: '#FFFFFF',
+  },
+  '&:active': {
+    borderColor: '#B00000',
+    backgroundColor: '#B00000',
+    color: '#FFFFFF',
+  }
 });
 export default BSCloseOutlinedButton;
--- BS-Web/Frontend-Core/src/components/Button/BSSaveOutlinedButton.js
+++ BS-Web/Frontend-Core/src/components/Button/BSSaveOutlinedButton.js
@@ -2,20 +2,22 @@ import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 
 const BSSaveOutlinedButton = styled(Button)({
- borderColor: '#0B9ED0',
+  borderColor: '#0B9ED0',
   color: '#0B9ED0',
   '&:hover': {
-    borderColor: '#0B9ED9',
-    color: '#0B9ED9',
-    backgroundColor: 'transparent'
+    borderColor: '#0B9ED0',
+    backgroundColor: '#0B9ED0',
+    color: '#FFFFFF',
   },
   '&.Mui-focusVisible': {
     borderColor: '#087FA8',
-    color: '#087FA8',
+    backgroundColor: '#087FA8',
+    color: '#FFFFFF',
   },
   '&:active': {
     borderColor: '#066788',
-    color: '#066788',
+    backgroundColor: '#066788',
+    color: '#FFFFFF',
   }
 });
 export default BSSaveOutlinedButton;
