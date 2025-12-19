820cf7877290f303c7f04a26be43d1c3fea22315
phayungsakp <phayungsak.p@oga.co.th>
2025-12-19 17:02:03 +0700

    feat: Add User Group management page and new dynamic BSDataGrid component for data display and CRUD operations.

--- BS-Web/Frontend-Core/src/pages/Authentication/UserGroup.js
+++ BS-Web/Frontend-Core/src/pages/Authentication/UserGroup.js
@@ -38,7 +38,7 @@ const UserGroupPage = (props) => {
           bsLocale={locale_id}
           bsPreObj="sec"
           bsObj="t_com_user_group"
-          bsCols="user_group_id,name,description,is_active,create_by,create_date,update_by,update_date"
+          bsCols="user_group_id,app_id,name,description,is_active,create_by,create_date,update_by,update_date"
           bsObjBy="name asc"
           bsShowDescColumn={false}
           bsComboBox={[
@@ -68,6 +68,13 @@ const UserGroupPage = (props) => {
           // onCheckBoxSelected={(rows) => {
           //   setSelectedRows(rows);
           // }}
+          bsColumnDefs={[
+            {
+              field: "app_id",
+              headerName: "Application",
+              width: 150,
+            },
+          ]}
         />
       </Paper>
     </>
