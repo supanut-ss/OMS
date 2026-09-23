c669497322369773da9950c85e8d42d967012741
PattrawutKhammaAot <140035503+PattrawutKhammaAot@users.noreply.github.com>
2026-09-23 09:19:00 +0700

    Update ProjectTeamRequest.cs

--- BS-API-Core/ApiCore/Models/Requests/ProjectTeamRequest.cs
+++ BS-API-Core/ApiCore/Models/Requests/ProjectTeamRequest.cs
@@ -1,14 +1,5 @@
 namespace ApiCore.Models.Requests
 {
-    public class AssignProjectTaskToTeamRequest
-    {
-        public int? project_task_member_id { get; set; }
-        public int project_task_id { get; set; }
-        public int project_header_id { get; set; }
-        public string? user_id { get; set; }
-        public decimal manday { get; set; }
-    }
-
     public class ProjectTeamRequest
     {
         public int? project_member_id { get; set; }
